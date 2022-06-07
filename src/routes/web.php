4bdd8ed65933ef1daef9d88031141642c848d882
加茂竜之介 <Kamoryu2001@gmail.com>
2022-06-07 19:33:01 +0900

    ルーディング完了

--- src/routes/web.php
+++ src/routes/web.php
@@ -10,8 +10,26 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{msg}', function ($msg) {
+    $html = <<<EOF
+    <html>
+    <head>
+    <title>Hello</title>
+    <style>
+        body {font-size:16pt; color:#999;}
+        h1 {font-size:100pt; text-align:right; color:#eee;
+        margin:-40px 0px -50px 0px;}
+    </style>
+    <body>
+        <h1>Hello</h1>
+        <p>{$msg}</p>
+        <p>これは、サンプルで作ったページです。</p>
+    </body>
+    </html>
+    EOF;
+
+    return $html;
 });
