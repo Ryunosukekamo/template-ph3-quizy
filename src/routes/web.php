a81077a4240416378b64bcbdc91a406b6c200c8f
加茂竜之介 <Kamoryu2001@gmail.com>
2022-06-07 22:42:15 +0900

    アクションを追加する

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,25 +11,29 @@
 |
 */
 
+// use Illuminate\Support\Facades\Route;
+
+// Route::get('/{msg}', function ($msg) {
+//     $html = <<<EOF
+//     <html>
+//     <head>
+//     <title>Hello</title>
+//     <style>
+//         body {font-size:16pt; color:#999;}
+//         h1 {font-size:100pt; text-align:right; color:#eee;
+//         margin:-40px 0px -50px 0px;}
+//     </style>
+//     <body>
+//         <h1>Hello</h1>
+//         <p>{$msg}</p>
+//         <p>これは、サンプルで作ったページです。</p>
+//     </body>
+//     </html>
+//     EOF;
+
+//     return $html;
+// });
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{msg}', function ($msg) {
-    $html = <<<EOF
-    <html>
-    <head>
-    <title>Hello</title>
-    <style>
-        body {font-size:16pt; color:#999;}
-        h1 {font-size:100pt; text-align:right; color:#eee;
-        margin:-40px 0px -50px 0px;}
-    </style>
-    <body>
-        <h1>Hello</h1>
-        <p>{$msg}</p>
-        <p>これは、サンプルで作ったページです。</p>
-    </body>
-    </html>
-    EOF;
-
-    return $html;
-});
+Route::get('','HelloController@index');
