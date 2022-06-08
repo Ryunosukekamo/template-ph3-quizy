7c2b7842b14b60e924a412600312edbccf2b6cf2
加茂竜之介 <Kamoryu2001@gmail.com>
2022-06-08 17:14:26 +0900

    シングルアクションコントローラー

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -6,16 +6,18 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function index($id='firstName',$pass='lastName'){
+    public function __invoke($id='firstName',$pass='lastName')
+    {
         $html = <<<EOF
         <html>
-        <head>
         <title>Hello</title>
+        <head>
         <style>
             body {font-size:16pt; color:#999;}
             h1 {font-size:100pt; text-align:right; color:#eee;
             margin:-40px 0px -50px 0px;}
         </style>
+        </head>
         <body>
             <h1>Index</h1>
             <p>これは、Helloコントローラーのindexアクションです。</p>
@@ -29,30 +31,4 @@ class HelloController extends Controller
 
         return $html;
     }
-    
-    public function other($id='firstName',$pass='lastName'){
-        $html = <<<EOF
-        <html>
-        <head>
-        <title>Hello</title>
-        <style>
-            body {font-size:16pt; color:#999;}
-            h1 {font-size:100pt; text-align:right; color:#eee;
-            margin:-40px 0px -50px 0px;}
-        </style>
-        <body>
-            <h1>other</h1>
-            <p>これは、Helloコントローラーのotherアクションです。</p>
-            <ul>
-                <li>ID:{$id}</li>
-                <li>PASS:{$pass}</li>
-            </ul>
-        </body>
-        </html>
-        EOF;
-
-        return $html;
-    }
 }
-
-
--- src/routes/web.php
+++ src/routes/web.php
@@ -36,5 +36,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('{id?}/{pass?}','HelloController@index');
-Route::get('other/{id?}/{pass?}','HelloController@other');
+Route::get('{id?}/{pass?}','HelloController');
