1bed6ac4a689bb733feda831ade94d0215bfbae8
加茂竜之介 <Kamoryu2001@gmail.com>
2022-06-08 16:45:09 +0900

    複数アクションの利用

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -29,7 +29,30 @@ class HelloController extends Controller
 
         return $html;
     }
-   
+    
+    public function other($id='firstName',$pass='lastName'){
+        $html = <<<EOF
+        <html>
+        <head>
+        <title>Hello</title>
+        <style>
+            body {font-size:16pt; color:#999;}
+            h1 {font-size:100pt; text-align:right; color:#eee;
+            margin:-40px 0px -50px 0px;}
+        </style>
+        <body>
+            <h1>other</h1>
+            <p>これは、Helloコントローラーのotherアクションです。</p>
+            <ul>
+                <li>ID:{$id}</li>
+                <li>PASS:{$pass}</li>
+            </ul>
+        </body>
+        </html>
+        EOF;
+
+        return $html;
+    }
 }
 
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -36,4 +36,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('{w?}/{e?}','HelloController@index');
+Route::get('{id?}/{pass?}','HelloController@index');
+Route::get('other/{id?}/{pass?}','HelloController@other');
