6a15bd0be49693ab99bdcc6494861d364c7e5ae9
加茂竜之介 <Kamoryu2001@gmail.com>
2022-06-12 20:55:30 +0900

    quizy見た目完了

--- src/resources/views/Todisplay/index.blade.php
+++ src/resources/views/Todisplay/index.blade.php
@@ -6,22 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>難読地名クイズ</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="{{ asset('/css/index.css') }}">
 </head>
 
 <body>
     <!-- 中央寄せ -->
-    @foreach($arraysForChoices[$id] as $choice)
-    <div class="parent">
-        <h2 class="name"> 1.この地名はなんて読む?</h2>
-        <img src="{{$choice[0]}}" alt="たかなわ">
-
-        <ul>
-            <li>{{ $choice[1] }}</li>
-            <li>{{ $choice[2] }}</li>
-            <li>{{ $choice[3] }}</li>
-        </ul>
-    </div>
-    @endforeach
+    <main>
+        @foreach($arraysForChoices[$id] as $choice)
+        <div class="question">
+            <h2 class="question_name"> この地名はなんて読む?</h2>
+            <img src="{{$choice[0]}}" alt="たかなわ">
+    
+            <ul>
+                <li class="choice">{{ $choice[1] }}</li>
+                <li class="choice">{{ $choice[2] }}</li>
+                <li class="choice">{{ $choice[3] }}</li>
+            </ul>
+        </div>
+        @endforeach
+    </main>
 
 </body>
