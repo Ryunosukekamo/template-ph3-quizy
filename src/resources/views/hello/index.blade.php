1ddf593e1d4f9d04099660aaa373ebd80f90875c
加茂竜之介 <Kamoryu2001@gmail.com>
2022-06-14 23:00:18 +0900

    ベースレイアウトと継承レイアウトの作成

--- src/resources/views/hello/index.blade.php
+++ src/resources/views/hello/index.blade.php
@@ -1,16 +1,17 @@
-<body>
-    <h1>Blade/index</h1>
-    <p>&#064;whileディレクティブの例</p>
-    <p>＊データ一覧</p>
-    @php
-    $counter = 0;
-    @endphp 
-    <ol>
-    @while($counter < count($data))
-    <li>{{$data[$counter]}}</li>
-    @php
-    $counter ++; 
-    @endphp    
-    @endwhile
-    </ol>
-</body>
+@extends('layouts.helloapp')
+
+@section('title','index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツです。</p>
+    <p>必要なだけ記述出来ます。</p>
+@endsection
+
+@section('footer')
+    copyright 2020 tuyano.
+@endsection
