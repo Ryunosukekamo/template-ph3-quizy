f0476c32b579cdf8cb3a05c6cde3517ac8994a28
加茂竜之介 <Kamoryu2001@gmail.com>
2022-06-14 22:21:25 +0900

    phpディレクティブについて

--- src/resources/views/hello/index.blade.php
+++ src/resources/views/hello/index.blade.php
@@ -1,11 +1,16 @@
 <body>
     <h1>Blade/index</h1>
-    <p>&#064;forディレクティブの例</p>
+    <p>&#064;whileディレクティブの例</p>
     <p>＊データ一覧</p>
-    @foreach ($data as $item)
-    <ul>
-    <li>No,{{$loop->iteration}}.{{$item}}</li>
-    </ul>
-    @endforeach    
-    <p>ーーここまで</p>
+    @php
+    $counter = 0;
+    @endphp 
+    <ol>
+    @while($counter < count($data))
+    <li>{{$data[$counter]}}</li>
+    @php
+    $counter ++; 
+    @endphp    
+    @endwhile
+    </ol>
 </body>
