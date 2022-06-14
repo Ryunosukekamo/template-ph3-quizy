4976c225b0ff913662abd6780d171dd5c33f5362
加茂竜之介 <Kamoryu2001@gmail.com>
2022-06-14 20:20:25 +0900

    繰り返しディレクティブを利用する

--- src/resources/views/hello/index.blade.php
+++ src/resources/views/hello/index.blade.php
@@ -1,22 +1,9 @@
-<head>
-    <title>Hello/index</title>
-    <style>
-        body{ font-size: 16pt; color: #999;}
-        h1{ font-size: 100pt; text-align: right; color: #f6f6f6;
-        margin: -50px 0px -100px 0px;}
-    </style>
-</head>
 <body>
     <h1>Blade/index</h1>
-    @isset ($msg)
-    <p>こんにちは、{{$msg}}さん。</p>
-    @else
-    <p>何か書いて下さい。</p>
-    @endisset
-    <form method="POST" action="">
-        @csrf
-        <input type="text" name="msg">
-        <input type="submit">
-    </form>
+    <p>&#064;foreachディレクティブの例</p>
+    <ol>
+        @foreach($data as $item)
+        <li>{{$item}}</li>
+        @endforeach
+    </ol>
 </body>
-</html>
