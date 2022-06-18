45013b7d9566b953f7a48305b3a234593c962301
加茂竜之介 <Kamoryu2001@gmail.com>
2022-06-18 14:29:05 +0900

    こういう書き方もできる

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
     public function index(){
-        return view('hello.index',['message'=>'Hello!']);
+        
+        $message = [
+            'message'=>'Hello!!'
+        ];
+
+        return view('hello.index',$message);
     }
 }
