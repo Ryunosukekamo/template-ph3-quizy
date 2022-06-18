516f2f70ea11f8115840e8f6bc6eb103aa8dc707
加茂竜之介 <Kamoryu2001@gmail.com>
2022-06-18 13:36:00 +0900

    ビューコンポーザーを利用する

--- src/resources/views/hello/index.blade.php
+++ src/resources/views/hello/index.blade.php
@@ -10,18 +10,6 @@
 
 @section('content')
     <p>ここが本文のコンテンツです。</p>
-    <p>必要なだけ記述出来ます。</p>
-    <ul>
-        @each('components.item',$data,'item')
-    </ul>
-
-    @component('components.message')
-        @slot('msg_title')
-        kamo
-        @endslot
-
-        @slot('msg_content')
-        ryu
-        @endslot
-    @endcomponent
+    <p>Controller value<br>'message'={{$message}}</p>
+    <p>ViewComposer value<br>'view_message'={{$view_message}}</p>
 @endsection
