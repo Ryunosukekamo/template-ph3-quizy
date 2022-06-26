df8badafa72df76a893e8ed14392b22b295c302c
加茂竜之介 <Kamoryu2001@gmail.com>
2022-06-26 14:47:32 +0900

    bigQuestionsのテーブル作成

---
+++ src/database/migrations/2022_06_26_054231_big_questions.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class BigQuestions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bigQuestions',function(Blueprint $table){
+            $table->increments('id');
+            $table->string('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
